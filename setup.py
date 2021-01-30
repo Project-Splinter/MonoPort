8cbdfb5227ce5d7bebec5ddd82b9691c015ea44e
Ruilong Li <ruilongl@usc.edu>
2021-01-29 21:49:15 -0800

    update setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='monoport',
+    url='',
+    description='',
+    version='1.0.0',
+    author='Ruilong Li',
+    author_email='ruilongl@usc.edu',
+    license='MIT License',
+    packages=setuptools.find_packages(),
+)
+
